5a8a8c8bab9a20cb3321d077e9524f4a958af2da
Samarth <Varahi1917115@gmail.com>
2022-04-11 10:43:09 -0400

    commit tree b352b3a881982f546a1a6e9aa7000a6cf8e94917

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+name: 'Samarth',
+streetAddress: '1262 NotMyReal St, Fishers, IN 60921'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+    const emp = {...employee };
+    emp[key] = value;
+
+    return emp;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
+    employee[key] = value;
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key)
+{
+    const empl = {...employee};
+    delete empl[key]
+    return empl
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+    delete employee[key]
+    return employee
+}
